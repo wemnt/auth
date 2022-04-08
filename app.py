4f86d553251a579d1da0672f1359bfe4d81dcd5a
 Артем Рязанов <wemnt@MacBook-Pro-Artem.local>
2022-04-08 19:07:31 +0400

    created post route

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
-from fastapi import FastAPI
-from fastapi.responses import FileResponse
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
 
 
 app = FastAPI()
+
+templates = Jinja2Templates(directory="views")
+
 
 
 @app.get("/")
@@ -10,9 +15,15 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/penis", response_class=FileResponse)
-async def penis():
+@app.get("/penis", response_class=HTMLResponse)
+async def penis(request: Request):
     html_content = "views/signin.html"
 
-    return FileResponse(html_content)
+    return templates.TemplateResponse("signin.html", {"request": request})
 
+
+@app.post("/login")
+async def read_login(email: str = Form(...), password: str = Form(...)):
+    form = {"email": email, "password": password}
+
+    return form
