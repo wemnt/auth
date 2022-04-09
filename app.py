2831ad353cf69d695a8d043f78863fa90a7c0739
 Артем Рязанов <wemnt@MacBook-Pro-Artem.local>
2022-04-10 00:20:30 +0400

    created requirements

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
-from fastapi import FastAPI, Request, Form
+from typing import List
+
+from fastapi import FastAPI, Request, Form, Depends
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
 
-
+from models.utils import as_form
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="views")
 
+class Test1(BaseModel):
+    a: str
+    b: int
 
+
+@as_form
+class Test(BaseModel):
+    param: str
+    test: List[Test1]
+    test1: Test1
+    b: int = 1
+    a: str = '2342'
 
 @app.get("/")
 async def root():
@@ -22,8 +36,6 @@ async def penis(request: Request):
     return templates.TemplateResponse("signin.html", {"request": request})
 
 
-@app.post("/login")
-async def read_login(email: str = Form(...), password: str = Form(...)):
-    form = {"email": email, "password": password}
-
-    return form
+@app.post("/penis", response_model=Test)
+async def read_login(request: Request, form: Test = Depends(Test.as_form)):
+    return await request.body()
